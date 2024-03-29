c934196c3d37bc825becfa4dc05c9a97f724c94b
amirmahdiramezani <amirmahdiramezani.techguy@gmail.com>
2024-03-29 18:35:57 +0430

    test

--- controller/validator.py
+++ controller/validator.py
@@ -2,4 +2,6 @@ import re
 
 
 def name_validator(name):
-    return bool(re.match("^[a-zA-Z\s]{3,30}$", name))
+    return bool(re.match(r"^[a-zA-Z\s]{3,30}$", name))
+def family_validator(family):
+    return bool(re.match(r""))
