87aee794c624ab5265ada534f2e9106903b63f80
amirmahdiramezani <amirmahdiramezani.techguy@gmail.com>
2024-03-29 18:19:50 +0430

    test

---
+++ controller/person_info.py
@@ -0,0 +1,15 @@
+from tikinter import *
+
+
+def submit():
+    id = id_entry.get()
+    name = name_entry.get()
+    family = family_entry.get()
+    company = company_entry.get
+    print("Id: ",id)
+    print("name: ",name)
+    print("family: ",family)
+    print("company: ",company)
+
+
+
